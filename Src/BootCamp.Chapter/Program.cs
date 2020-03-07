d03b72b3895c8d7e0496fa63034f55e2d81c9adf
mahdi <mahdi@LAPTOP-K728P1AM>
2020-03-08 00:11:58 +0100

    lets try

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -14,6 +14,7 @@ namespace BootCamp.Chapter
             //BMI calculations
             double bmi = weight * 703 / height / height;
             Console.WriteLine(bmi);
+            //Code
         }
     }
 }
